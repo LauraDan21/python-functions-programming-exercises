9d820228efa08d14c8ce7fca2119917bcb8e50ce
LauraDan21 <161901568+LauraDan21@users.noreply.github.com>
2025-03-22 16:21:49 +0000

    function exercises

--- .learn/resets/06-lambda-function-two/app.py
+++ .learn/resets/06-lambda-function-two/app.py
@@ -1,4 +1,5 @@
-rapid = lambda n: n[:-1]
+
+
 
 # Your code above, please do not change code below
 print(rapid("bob")) # Should print "bo"
--- exercises/08-Function-parameters/app.py
+++ exercises/08-Function-parameters/app.py
@@ -1,6 +1,6 @@
 # Your code goes here:
-def render_person(param):
-    return param
+def render_person(name, birthay, color, age, gender):
+    return f"{name} is a {age} years old {gender} born in {birthay} with {color} eyes"
 
 
 # Do not edit below this line
